74612671aa0989433247091d2b44b3e0e9679b72
Eric Kelly <heroiceric@gmail.com>
2016-01-06 21:09:49 -0500

    Don't specify username/pass for test db

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :opencast, Opencast.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "opencast_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
