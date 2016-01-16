b39fd7a4f776c039dc1cf29cd878cd1def23abb3
Eric Kelly <heroiceric@gmail.com>
2016-01-16 17:53:20 -0500

    Add Episode model

--- lib/opencast/factory.ex
+++ lib/opencast/factory.ex
@@ -1,7 +1,17 @@
 defmodule Opencast.Factory do
   use ExMachina.Ecto, repo: Opencast.Repo
 
+  alias Opencast.Episode
   alias Opencast.Podcast
+
+  def factory(:episode) do
+    %Episode{
+      podcast: build(:podcast),
+      published_at: Ecto.DateTime.utc(),
+      url: sequence(:feed_url, &"http://example#{&1}.com/rss"),
+      title: sequence(:title, &"Awesome Episode ##{&1}")
+    }
+  end
 
   def factory(:podcast) do
     %Podcast{
