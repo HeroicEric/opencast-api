1c5e18164d92c98b468eae17b0d70bf375141129
Eric Kelly <heroiceric@gmail.com>
2019-07-05 14:50:22 -0700

    Replace deprecated String.strip/1 with String.trim/1
    Fixes the following deprecation:
    
    ```
    warning: String.strip/1 is deprecated. Use String.trim/1 instead
      lib/audrey/lib/util.ex:3
    ```

--- lib/audrey/lib/util.ex
+++ lib/audrey/lib/util.ex
@@ -1,6 +1,6 @@
 defmodule Audrey.Util do
   def format_value(value) do
-    value |> String.strip() |> value_or_nil
+    value |> String.trim() |> value_or_nil
   end
 
   def value_or_nil(""), do: nil
